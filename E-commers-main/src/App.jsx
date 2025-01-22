9170f52f16726f4ebe0013c3d4a388779fae5a54
Mahmoud Saaed Romeh <123378237+Engromeh@users.noreply.github.com>
2025-01-22 15:29:30 +0200

    first commit

---
+++ E-commers-main/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import CardProducte from './Component/CardProducte/CardProducte'
+import Navbar from './Component/Navbar/Navbar'
+import Slider from './Component/Slider/Slider'
+
+function App() {
+
+  return (
+    <>
+<Navbar />
+<Slider />
+<CardProducte />
+    </>
+  )
+}
+
+export default App
---
+++ E-commers-main/src/Component/Cardsst/Cardsst.jsx
@@ -0,0 +1,30 @@
+
+// const Cardsst = (props) => {
+  
+// const {proudcet}=props
+// return (
+//     <>
+
+
+//       <div className="card" style={{ width: "18rem" }}>
+//         <img
+//           src={proudcet.image}
+//           className="card-img-top"
+//           alt="Sample Image"
+//         />
+//         <div className="card-body">
+//           <h5 className="card-title">{proudcet.name}</h5>
+//           <p className="card-text">
+//           {proudcet.description}
+//           </p>
+//           <a href="#" className="btn btn-primary">
+//             Go somewhere
+//           </a>
+//         </div>
+//       </div>
+//     </>
+//   );
+
+// };
+
+// export default Cardsst;
